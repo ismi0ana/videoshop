3ddaf88407007d952eec984c404e881b99ca5c3f
ismi0ana <Almehdichouari@outlook.com>
2026-05-05 00:52:02 +0200

    Update product.java

--- src/main/java/videoshop/product.java
+++ src/main/java/videoshop/product.java
@@ -1,5 +1,7 @@
-// Beispiel aus dem Bereich 'catalog'
+package videoshop.catalog;
+
 import org.salespointframework.catalog.Product;
+import org.javamoney.moneta.Money;
 import jakarta.persistence.Entity;
 
 @Entity
@@ -8,17 +10,26 @@ public class Disc extends Product {
     public static enum DiscType { DVD, BLURAY; }
 
     private DiscType type;
+    private int allowedAge; // VERBESSERUNG: Neues Feld für funktionale Logik
 
     @SuppressWarnings("unused")
-    private Disc() {} // Für JPA
+    private Disc() {}
 
-    public Disc(String name, String image, Money price, DiscType type) {
+    // Konstruktor mit neuem Parameter allowedAge
+    public Disc(String name, String image, Money price, DiscType type, int allowedAge) {
         super(name, price);
         this.type = type;
         this.addImage(image);
+        this.allowedAge = allowedAge;
     }
 
     public DiscType getType() {
         return type;
+    }
+
+    public int getAllowedAge() {
+        return allowedAge;
+    }
+}
     }
 }
